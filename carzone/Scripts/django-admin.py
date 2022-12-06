a0e262525d34d41617de5ee9d984625d3c36c757
Crowten2020 <lawrence.gavs@gmail.com>
2022-12-06 17:40:57 +0530

    First commit

---
+++ carzone/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\LawrenceJacob\OneDrive - Ahana Systems and Solutions (P) Ltd\Desktop\django-python\carzone\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
